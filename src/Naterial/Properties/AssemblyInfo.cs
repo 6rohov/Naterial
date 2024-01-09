89bbf226c354b6e491263fa31b3a7990dafa0cf9
6rohov <156028594+6rohov@users.noreply.github.com>
2024-01-10 01:27:08 +0300

    initial: adjust project structure

---
+++ src/Naterial/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://schemas.6rohov.com/naterial", "nd")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
